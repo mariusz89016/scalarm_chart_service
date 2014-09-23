4a7a6471bd3da71e2ad0ece0f1cfedf47947db50
Grazyna <grazyna@plgubuntu.(none)>
2014-09-23 14:08:43 +0200

    First implementation of grouping analyses

--- config.js
+++ config.js
@@ -1,13 +1,32 @@
 var config = {
-	title: "MOJ TYTUL HAML",
-	methods: [{
-		name: "Interaction",
-		id: 12,
-		url: "interactionModal"
-	},{
-		name: "Pareto",
-		id: 34,
-		url: "paretoModal"
+	groups: [
+	{
+		id: "nasze_2",
+		name: "Nasze 2",
+		methods: [{
+			name: "Interaction",
+			id: "interactionModal",
+			image: "http://placehold.it/117x71",
+			description: "Shows interaction between 2 input parameters"
+		},{
+			name: "Pareto",
+			id: "paretoModal",
+			image: "http://placehold.it/117x71",
+			description: "Shows significane of parameters (or interaction)"
+		}],
+		
+	},
+	{
+		id: "pozostale",
+		name: "Pozostałe",
+		methods: [{
+			name: "Bivariate analysis",
+			id: "experiment-analysis-modal",
+			image: "http://placehold.it/117x71",
+			description: "Bivariate analysis - scatter plot"
+
+		}]
+		
 	}],
 	pretty: true
 };

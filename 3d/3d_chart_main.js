50b549135324857f1d11b7a047b21e233c179e1f
Mariusz <mariusz89016@plgubuntu.(none)>
2014-10-10 13:16:43 +0200

    Mock implementation of 3d chart (not working!)
    TODO: check Highcharts version in Scalarm EM

---
+++ 3d/3d_chart_main.js
@@ -0,0 +1,66 @@
+function threeD_main(i, data) {
+    // Give the points a 3D feel by adding a radial gradient
+    
+
+    // Set up the chart
+	var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: $('#three_d_chart_'+ i + " .chart")[0],
+            margin: [100,0,0,0],
+            type: 'scatter',
+            options3d: {
+                enabled: true,
+                alpha: 10,
+                beta: 30,
+                depth: 250,
+                viewDistance: 5,
+
+                frame: {
+                    bottom: { size: 1, color: 'rgba(0,0,0,0.02)' },
+                    back: { size: 1, color: 'rgba(0,0,0,0.04)' },
+                    side: { size: 1, color: 'rgba(0,0,0,0.06)' }
+                }
+            }
+        },
+        title: {
+            text: 'Draggable box'
+        },
+        subtitle: {
+            text: 'Click and drag the plot area to rotate in space'
+        },
+        plotOptions: {
+            scatter: {
+                width: 10,
+                height: 10,
+                depth: 10
+            }
+        },
+        yAxis: {
+            min: 0,
+            max: 10,
+            title: null
+        },
+        xAxis: {
+            min: 0,
+            max: 10,
+            gridLineWidth: 1
+        },
+        zAxis: {
+            min: 0,
+            max: 10
+        },
+        legend: {
+            enabled: false
+        },
+        series: [{
+            name: 'Reading',
+            colorByPoint: true,
+            data: [[1,2,3],[1,2,4]]
+        }]
+    });
+	console.log(chart.margin);
+
+
+    // Add mouse events for rotation
+    
+}

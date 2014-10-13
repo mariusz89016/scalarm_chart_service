5c6d2677db59ac9b86d5cac3001c288202651bb9
Grazyna <grazyna.j.skiba@gmail.com>
2014-10-13 19:39:42 +0200

    Small fix after merging (in panel_locals.js)

--- panel_locals.js
+++ panel_locals.js
@@ -6,14 +6,19 @@ module.exports = {
             methods: [{
                 name: "Interaction",
                 id: "interactionModal",
-                image: "http://placehold.it/117x71",
+                image: "https://placehold.it/117x71",
                 description: "Shows interaction between 2 input parameters"
             },{
                 name: "Pareto",
                 id: "paretoModal",
-                image: "http://placehold.it/117x71",
+                image: "https://placehold.it/117x71",
                 description: "Shows significance of parameters (or interaction)"
-            }],
+            },{
+                name: "3d scatter plot",
+                id: "threeDModal",
+                image: "https://placehold.it/117x71",
+                description: "TODO"
+            }]
 
         },
         {
@@ -30,14 +35,14 @@ module.exports = {
                 name: "Regression trees",
                 id: "experiment-analysis-modal",
                 em_class: "rtree-analysis",
-                image: "http://placehold.it/117x71",
+                image: "https://placehold.it/117x71",
                 description: "TODO"
 
             },{
                 name: "Scatter plots",
                 id: "experiment-analysis-modal",
                 em_class: "bivariate-analysis",
-                image: "http://placehold.it/117x71",
+                image: "https ://placehold.it/117x71",
                 description: "Bivariate analysis - scatter plot"
 
             }]
